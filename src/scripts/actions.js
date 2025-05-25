cb72548dbe6eacdbe18c78327e35c46eaef598e2
Compdog Inc. <compdog.inc@gmail.com>
2025-05-25 13:51:26 -0400

    Add level selector

--- src/scripts/actions.js
+++ src/scripts/actions.js
@@ -1,8 +1,9 @@
 class Action {
-    constructor(name, checkCondition, id) {
+    constructor(name, checkCondition, id, level) {
         this.name = name;
         this.checkCondition = checkCondition;
         this.id = id;
+        this.level = level;
     }
 
     wasPerformed(...args) {
@@ -16,21 +17,21 @@ const check = (e) => {
     return true;
 };
 const actions = [
-    new Action('Climb', check, 'B7'),
-    new Action('Auto Score', check, 'B1'),
-    new Action('Ground intake', check, 'B2'),
-    new Action('Grab Algae', check, 'B3'),
-    new Action('Manual Elevator Up', check, 'POV+Y'),
-    new Action('Manual Elevator Down', check, 'POV-Y'),
-    new Action('Manual Arm Up', check, 'POV+X'),
-    new Action('Manual Arm Down', check, 'POV-X'),
-    new Action('Ground Intake Relative', check, 'B10'),
-    new Action('Elevator Relative', check, 'B8'),
-    new Action('Climb Relative', check, 'B12'),
-    new Action('Pose Rotation Reset', check, 'B5'),
-    new Action('Reset To Limelight', check, 'B6'),
-    new Action('Kill Subsystems', check, 'B4'),
-    new Action('Speed Level', check, 'Axis4')
+    new Action('Climb', check, 'B7', 0),
+    new Action('Auto Score', check, 'B1', 0),
+    new Action('Ground intake', check, 'B2', 0),
+    new Action('Grab Algae', check, 'B3', 0),
+    new Action('Manual Elevator Up', check, 'POV+Y', 1),
+    new Action('Manual Elevator Down', check, 'POV-Y', 1),
+    new Action('Manual Arm Up', check, 'POV+X', 1),
+    new Action('Manual Arm Down', check, 'POV-X', 1),
+    new Action('Ground Intake Relative', check, 'B10', 1),
+    new Action('Elevator Relative', check, 'B8', 1),
+    new Action('Climb Relative', check, 'B12', 0),
+    new Action('Pose Rotation Reset', check, 'B5', 2),
+    new Action('Reset To Limelight', check, 'B6', 2),
+    new Action('Kill Subsystems', check, 'B4', 2),
+    new Action('Speed Level', check, 'Axis4', 1)
 ];
 
 export { Action, actions };
